d50d7d02f0c60b2d35a43db04d13c03eb0b803e5
SeHIgh <sehi0119@naver.com>
2024-07-09 10:52:34 +0900

    Auto Upload

--- docs/assets/js/Union.js
+++ docs/assets/js/Union.js
@@ -1,5 +1,5 @@
 async function getApiKey() {
-    const response = await fetch('../../config.json');
+    const response = await fetch('./config.json');
     const config = await response.json();
     return config.maple_apiKey;
 }
@@ -23,7 +23,37 @@ async function lookupCharacterOcid() {
 
         const data = await response.json();
         document.getElementById('result').innerText = `OCID: ${data.ocid}`;
+        return data.ocid;
     } catch (error) {
         document.getElementById('result').innerText = `Error: ${error.message}`;
+    }
+}
+
+async function lookupCharacterInfo() {
+    const ocid = await lookupCharacterOcid();
+    const apiKey = await getApiKey();
+    const url = `https://open.api.nexon.com/maplestory/v1/character/basic?ocid=${ocid}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'x-nxopen-api-key': apiKey
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        const data = await response.json();
+        document.getElementById('characterImgResult').src = `${data.character_image}`;
+        document.getElementById('serverResult').innerText = `${data.world_name}`;
+        document.getElementById('characterNameResult').innerText = `${data.character_name}`;
+        document.getElementById('jobResult').innerText = `${data.character_class}`;
+        document.getElementById('levelResult').innerText = `${data.character_level}`;
+        document.getElementById('expResult').innerText = `${data.character_exp_rate}`;
+    } catch (error) {
+        document.getElementById('characterNameResult').innerText = `Error: ${error.message}`;
     }
 }
