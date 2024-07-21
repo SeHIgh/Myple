5ddec1e87f494b4ac06159bcff18390b782948ff
SeHyeon <sehi@naver.com>
2024-07-21 16:44:46 +0900

    Window Auto Upload

--- docs/assets/js/weeklyQuest.js
+++ docs/assets/js/weeklyQuest.js
@@ -58,28 +58,48 @@ document.addEventListener('DOMContentLoaded', function() {
     const imageContainers = document.querySelectorAll('.slide');
 
     imageContainers.forEach(container => {
+        const anchor = container.querySelector('a');
         const img = container.querySelector('a>img');
         const defaultSrc = img.src;
         const hoverSrc = container.getAttribute('data-hover');
         const clickSrc = container.getAttribute('data-click');
 
-        container.addEventListener('mouseenter', () => {
-            img.src = clickSrc;
-        });
+        if(anchor.classList.contains('ready')) {
+            container.addEventListener('mouseenter', () => {
+                img.src = hoverSrc || clickSrc; // hoverSrc가 없을 경우 clickSrc 사용
+            });
+    
+            container.addEventListener('mouseleave', () => {
+                img.src = defaultSrc;
+            });
+    
+            container.addEventListener('mousedown', () => {
+                img.src = clickSrc;
+                img.style.position = 'relative';
+                img.style.top = '2px';
+            });
 
-        container.addEventListener('mouseleave', () => {
-            img.src = defaultSrc;
-        });
+            container.addEventListener('mouseup', () => {
+                img.src = clickSrc;
+                img.style.position = 'relative';
+                img.style.top = '0';
+            });
+        } else if (anchor.classList.contains('not-ready')) {
+            anchor.addEventListener('click', (event) => {
+                event.preventDefault();
+                alert("준비중입니다. 다른 컨텐츠를 이용해주세요.");
+            });
+        }
+    });
+});
 
-        container.addEventListener('mousedown', () => {
-            img.src = clickSrc;
-            img.style.position = 'relative';
-            img.style.top = '2px';
-        });
-        container.addEventListener('mouseup', () => {
-            img.src = clickSrc;
-            img.style.position = 'relative';
-            img.style.top = '0';
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    const notReadyAnchors = document.querySelectorAll('.slide a.not-ready');
+
+    notReadyAnchors.forEach(anchor => {
+        const img = anchor.querySelector('img');
+        if (img) {
+            img.style.filter = 'grayscale(100%)';
+        }
     });
 });
